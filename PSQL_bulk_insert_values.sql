7787c28e7318a1e484b74473c1d6da47bae87256
Simon <109409780+simonkapteina@users.noreply.github.com>
2022-10-21 15:51:37 +0800

    Delete PSQL_bulk_insert_values.sql

--- PSQL_bulk_insert_values.sql
+++
@@ -1,7 +0,0 @@
--- bulk insert values into shippers 
-
-INSERT INTO shippers(shipper_id,company_name,phone)
-VALUES 
-(10, 'X-Press','(503) 777-1818'),
-(11, 'Delivery Heros','(503) 333-6661'),
-(12, 'Shipping 4 U','(503) 555-3434');
